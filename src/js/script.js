fedecb4554c981f54f09e5e5a667a419d4f907d3
Alex <sasha-a@ukr.net>
2020-11-15 21:46:54 +0200

    Created section 4: Carousel on pure JS with dots

---
+++ src/js/script.js
@@ -0,0 +1,125 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    // slider #1
+    // const slides = document.querySelectorAll('.carousel__inner > div'),
+    //       next = document.querySelector('.carousel__next'),
+    //       prev = document.querySelector('.carousel__prev');
+
+    // let slideIndex = 1;
+
+    // function showSlides(n) {
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     }
+
+    //     slides.forEach(item => {
+    //         item.classList.add('hide');
+    //         item.classList.remove('show');
+    //     });
+
+    //     slides[slideIndex - 1].classList.add('show', 'fade');
+    //     slides[slideIndex - 1].classList.remove('hide');
+    // }
+    // showSlides(slideIndex);
+
+    // function plusSlides(n) {
+    //     showSlides(slideIndex +=n);
+    // }
+
+    // prev.addEventListener('click', () => {
+    //     plusSlides(-1);
+    // });
+    // next.addEventListener('click', () => {
+    //     plusSlides(1);
+    // });
+
+    // slider #2
+    const slider = document.querySelector('.carousel'),
+          slides = document.querySelectorAll('.carousel__slide'),
+          next = document.querySelector('.carousel__next'),
+          prev = document.querySelector('.carousel__prev'),
+          slidesWrapper = document.querySelector('.carousel__wrapper'),
+          slidesField = document.querySelector('.carousel__inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
+    
+    let slideIndex = 1,
+        offset = 0;
+
+    slidesField.style.width = 100 * slides.length + '%';
+    
+    slides.forEach(item => {
+        item.style.width = width;
+    });
+
+    // dots
+    const indicators = document.createElement('ol');
+    indicators.classList.add('carousel__indicators');
+    slider.append(indicators);
+    let dots = [];
+
+    for(let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.classList.add('carousel__dot');
+        dot.setAttribute('data-slide-to', i+1);
+        indicators.append(dot);
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        dots.push(dot);
+    }
+
+    function currentDot() {
+        dots.forEach(item => item.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
+
+    dots.forEach(item => {
+        item.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            
+            slidesField.style.transform = `translateX(-${offset}px)`;
+            
+            currentDot();
+        });
+    });
+
+    // arrow next
+    next.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length -2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length -2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        currentDot();
+    });
+    // arrow prev
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        currentDot();
+    });
+});
